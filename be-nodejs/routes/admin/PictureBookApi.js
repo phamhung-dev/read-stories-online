8f0b3ed2049c4b99c55ca3c532445b30d4b53e0d
pham-hung-25-03-01 <phamhunggl721@gmail.com>
2023-04-09 00:52:48 +0700

    TLNJ-38 CRUD PictureBook

--- be-nodejs/routes/admin/PictureBookApi.js
+++ be-nodejs/routes/admin/PictureBookApi.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const pictureBookApi = express.Router();
 const pictureBookController = require("./../../src/controllers/PictureBookController");
+const { fileUploader } = require("./../../config/CloudinaryConfig");
 
 pictureBookApi.get("/", pictureBookController.findAll);
-pictureBookApi.post("/create", pictureBookController.create);
+pictureBookApi.post("/create", fileUploader.single('avatar'), pictureBookController.create);
+pictureBookApi.put("/:id/update", fileUploader.single('avatar'), pictureBookController.update);
+pictureBookApi.get("/:id", pictureBookController.show);
+pictureBookApi.delete("/:id/destroy", pictureBookController.destroy);
 
 module.exports = pictureBookApi;

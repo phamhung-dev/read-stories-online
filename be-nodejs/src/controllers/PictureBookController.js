506cb24aea4c380cbc8c8bebe1ef5d39ec13998c
pham-hung-25-03-01 <phamhunggl721@gmail.com>
2023-04-09 00:52:48 +0700

    TLNJ-38 CRUD PictureBook

--- be-nodejs/src/controllers/PictureBookController.js
+++ be-nodejs/src/controllers/PictureBookController.js
@@ -1,4 +1,7 @@
 const pictureBookService = require('./../services/PictureBookService');
+const { cloudinary } = require("./../../config/CloudinaryConfig");
+
+const getPublicId = (imageURL) => imageURL.split("/").pop().split(".")[0];
 
 async function findAll(req, res) {
     try{
@@ -22,7 +25,39 @@ async function findAllPublished(req, res) {
 
 async function create(req, res) {
     try {
+        if(req.file){
+            req.body.avatar = req.file.path;
+        }
         const pictureBook = await pictureBookService.create(req.body);
+        res.status(200).json({message: 'SUCCESS', data: pictureBook});
+    }
+    catch (err) {
+        if(req.file){
+            await cloudinary.uploader.destroy(getPublicId(req.file.path));
+        }
+        res.status(400).json({message: 'ERROR', data: {content: err.message}});
+    }
+}
+
+async function update(req, res) {
+    try {
+        if(req.file){
+            req.body.avatar = req.file.path;
+        }
+        const pictureBook = await pictureBookService.update(req.params.id, req.body);
+        res.status(200).json({message: 'SUCCESS', data: pictureBook});
+    }
+    catch (err) {
+        if(req.file){
+            await cloudinary.uploader.destroy(getPublicId(req.file.path));
+        }
+        res.status(400).json({message: 'ERROR', data: {content: err.message}});
+    }
+}
+
+async function show(req, res) {
+    try {
+        const pictureBook = await pictureBookService.show(req.params.id);
         res.status(200).json({message: 'SUCCESS', data: pictureBook});
     }
     catch (err) {
@@ -30,4 +65,14 @@ async function create(req, res) {
     }
 }
 
-module.exports = { findAll, findAllPublished, create };
+async function destroy(req, res) {
+    try {
+        const pictureBook = await pictureBookService.destroy(req.params.id);
+        res.status(200).json({message: 'SUCCESS', data: pictureBook});
+    }
+    catch (err) {
+        res.status(400).json({message: 'ERROR', data: {content: err.message}});
+    }
+}
+
+module.exports = { findAll, findAllPublished, create, update, show, destroy };

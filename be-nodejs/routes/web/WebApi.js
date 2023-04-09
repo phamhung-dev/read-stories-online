f50019bc5bb6bcd29e327f5e0f48d5a74dc42346
pham-hung-25-03-01 <phamhunggl721@gmail.com>
2023-04-09 11:21:40 +0700

    TLNJ-46 CRUD Purchase

--- be-nodejs/routes/web/WebApi.js
+++ be-nodejs/routes/web/WebApi.js
@@ -2,8 +2,10 @@ const express = require("express");
 const webApi = express.Router();
 const pictureBookApi = require("./PictureBookApi");
 const userApi = require("./UserApi");
+const purchaseApi = require("./PurchaseApi");
 
 webApi.use("/picture-books", pictureBookApi);
 webApi.use("/user", userApi);
+webApi.use("/purchases", purchaseApi);
 
 module.exports = webApi;
--- be-nodejs/src/services/PurchaseService.js
+++ be-nodejs/src/services/PurchaseService.js
@@ -1,2 +1,64 @@
 const Purchase = require("./../models/Purchase");
 const { cleanProperties } = require('./../validator/RequestValidate');
+
+const populateQuery = [
+    { path: "pictureBook", select: ["name", "slug"] },
+    { path: "user", select: "name" }
+];
+
+async function findAll() {
+    try {
+        return await Purchase.find({}).populate(populateQuery);
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+async function findById(id) {
+    try {
+        return await Purchase.findById(id).populate(populateQuery);
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+async function findByUser(userId) {
+    try {
+        return await Purchase.find({ user: userId }).populate(populateQuery);
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+async function findByPictureBook(pictureBookId) {
+    try {
+        return await Purchase.find({ pictureBook: pictureBookId }).populate(populateQuery);
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+async function update(id, data){
+    allowedProperties = {
+        status: true
+    };
+    dataClean = cleanProperties(data, allowedProperties);
+    var purchase = await Purchase.findById(id);
+    if (!purchase) {
+        throw new Error("Purchase not found");
+    }
+    try {
+        purchase.status = dataClean.status;
+        await purchase.save();
+        return purchase.populate(populateQuery);
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+module.exports = { findAll, findById, findByUser, findByPictureBook, update };

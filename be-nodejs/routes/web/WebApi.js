34f91cc06050b8cb0975a31d64a7ff51dc082809
pham-hung-25-03-01 <phamhunggl721@gmail.com>
2023-04-09 15:35:45 +0700

    TLNJ-44 CRUD History

--- be-nodejs/routes/web/WebApi.js
+++ be-nodejs/routes/web/WebApi.js
@@ -3,9 +3,11 @@ const webApi = express.Router();
 const pictureBookApi = require("./PictureBookApi");
 const userApi = require("./UserApi");
 const purchaseApi = require("./PurchaseApi");
+const historyApi = require("./HistoryApi");
 
 webApi.use("/picture-books", pictureBookApi);
 webApi.use("/user", userApi);
 webApi.use("/purchases", purchaseApi);
+webApi.use("/histories", historyApi);
 
 module.exports = webApi;

94419ff87f33dbba4a3353c5e50f214609fc2a20
honhuthao <honhuthao001@gmail.com>
2023-04-09 15:46:02 +0700

    TLNJ-47 CRUD Role-User

--- be-nodejs/routes/admin/AdminApi.js
+++ be-nodejs/routes/admin/AdminApi.js
@@ -5,11 +5,13 @@ const authorApi = require("./AuthorApi");
 const chapterApi = require("./ChapterApi");
 const userApi = require("./UserApi");
 const purchaseApi = require("./PurchaseApi");
+const roleUserApi = require("./RoleUserApi");
 
 adminApi.use("/picture-books", pictureBookApi);
 adminApi.use("/chapters", chapterApi);
 adminApi.use("/users", userApi);
 adminApi.use("/purchases", purchaseApi);
 adminApi.use("/authors", authorApi);
+adminApi.use("/role-users", roleUserApi);
 
 module.exports = adminApi;

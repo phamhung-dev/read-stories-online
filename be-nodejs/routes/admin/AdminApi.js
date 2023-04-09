cf47358c325e852e661c6071d6af1d9715f2380f
Nhan9999 <trungnhan266@gmail.com>
2023-04-09 14:12:10 +0700

    TLNJ-39 CRUD Author

--- be-nodejs/routes/admin/AdminApi.js
+++ be-nodejs/routes/admin/AdminApi.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const adminApi = express.Router();
 const pictureBookApi = require("./PictureBookApi");
+const authorApi = require("./AuthorApi");
+
 
 adminApi.use("/picture-books", pictureBookApi);
+adminApi.use("/authors", authorApi);
+
 
 module.exports = adminApi;
--- be-nodejs/src/services/AuthorService.js
+++ be-nodejs/src/services/AuthorService.js
@@ -1,2 +1,65 @@
 const Author = require("./../models/Author");
 const { cleanProperties } = require('./../validator/RequestValidate');
+
+async function findAll() {
+    try {
+        return await Author.find({});
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+async function findBySlug(slug) {
+    try {
+      var author = await Author.findOne({ slug: slug });
+      if (!author) {
+        throw new Error('Author not found');
+      }
+      return author;
+    } catch (err) {
+      throw err;
+    }
+  }
+  
+async function create(data) {
+    allowedProperties = {
+        name: true,
+    };
+    dataClean = cleanProperties(data, allowedProperties);
+    var newAuthor = new Author(dataClean);
+    try {
+        var author = await Author.findOne({ name: dataClean.name });
+        if (author) {
+            throw new Error('Author name already exists');
+        }
+        await newAuthor.save();
+        return newAuthor;
+    }
+    catch (err) {
+        throw err;
+    }
+}
+
+async function update(id, data) {
+    allowedProperties = {
+        name: true,
+    };
+    dataClean = cleanProperties(data, allowedProperties);
+    try {
+        var author = await Author.findById(id);
+        if (!author) {
+            throw new Error('Author not found');
+        }
+        author = await Author.findOne({ name: dataClean.name });
+        if (author) {
+            throw new Error('Author name already exists');
+        }
+        return await Author.findOneAndUpdate({ _id: id }, dataClean, { new: true });
+    } catch (err) {
+        throw err;
+    }
+}
+
+
+module.exports = { findAll, findBySlug, create, update };
